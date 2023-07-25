4a0788ece28e6037fb1750cdf7ce15e1b0d4a046
Alejjaandro <alejandroolaso98@gmail.com>
2023-07-25 18:12:20 +0200

    Navbar component

---
+++ Frontend/src/components/Navbar.js
@@ -0,0 +1,59 @@
+import { Container, Button, Nav, Navbar as NavbarBS } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <NavbarBS sticky='top' className='bg-white shadow-sm mb-3'>
+            <Container >
+                <Nav className='me-auto'>
+
+                    <Nav.Link to="/" as={NavLink}>
+                        Home
+                    </Nav.Link>
+
+                    <Nav.Link to="/store" as={NavLink}>
+                        Store
+                    </Nav.Link>
+
+                    <Nav.Link to="/about" as={NavLink}>
+                        About
+                    </Nav.Link>
+
+                </Nav>
+
+                <Button
+                    style={{ with: "3rem", height: "3rem" }}
+                    variant='outline-primary'
+                    className='rounded-circle'
+                >
+                    {/* svg icon imported from https://iconsvg.xyz/ */}
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="#000000"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <circle cx="10" cy="20.5" r="1" />
+                        <circle cx="18" cy="20.5" r="1" />
+                        <path d="M2.5 2.5h3l2.7 12.4a2 2 0 0 0 2 1.6h7.7a2 2 0 0 0 2-1.6l1.6-8.4H7.1" />
+                    </svg>
+                    
+                    {/* For the number of the number of items on carts */}
+                    <div 
+                    className='rounded-circle bg-danger d-flex 
+                    justify-content-center align-items-center'
+                    style={{ 
+                        color: "white", 
+                        with: "1.5rem", 
+                        height: "1.5rem", 
+                        tranform: "translate(25%, 25%)"
+                    }}
+                    >3</div>
+                </Button>
+            </Container>
+        </NavbarBS>
+    )
+}
